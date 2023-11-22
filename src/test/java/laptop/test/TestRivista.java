2f118d0a114e8661292499c2693d9d135542b6e6
ilciro <danielecinus10@gmail.com>
2023-11-22 13:56:48 +0100

    linux v6 test libro and rivista

---
+++ src/test/java/laptop/test/TestRivista.java
@@ -0,0 +1,142 @@
+package laptop.test;
+
+import com.itextpdf.text.DocumentException;
+import laptop.controller.*;
+import laptop.database.RivistaDao;
+import laptop.exception.IdException;
+import laptop.model.raccolta.Rivista;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TestRivista {
+    private  final ResourceBundle RBBOOKCATEGORIES = ResourceBundle.getBundle("configurations/magazineCategories");
+    private final ResourceBundle RBRIVISTADAINSERIRE=ResourceBundle.getBundle("configurations/rivistaDaInserire");
+   private final ResourceBundle RBSETTAOGGETTO=ResourceBundle.getBundle("configurations/settaOggetto");
+    private final ResourceBundle REBARTADICREDITO=ResourceBundle.getBundle("configurations/cartaCredito");
+    private  final ResourceBundle RBFATTURA=ResourceBundle.getBundle("configurations/fattura");
+
+
+
+    private static final ControllerSystemState vis= ControllerSystemState.getInstance();
+    private final String[] info=new String[7];
+
+    private final ControllerAggiungiPage cAP=new ControllerAggiungiPage();
+    private final ControllerAcquista cA=new ControllerAcquista();
+    private final ControllerDownload cD=new ControllerDownload();
+    private final ControllerModifPage cMP=new ControllerModifPage();
+    private final ControllerPagamentoCC cPCC=new ControllerPagamentoCC();
+    private final ControllerCompravendita cC=new ControllerCompravendita();
+    private final ControllerPagamentoCash cPCash=new ControllerPagamentoCash();
+
+
+
+    private final Rivista r=new Rivista();
+
+    private final RivistaDao rD=new RivistaDao();
+
+    @Test
+    void testCategorieR()
+    {
+
+
+        Enumeration<String> keys = RBBOOKCATEGORIES.getKeys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement().replace("=","");
+            r.setTipologia(key);
+        }
+        assertNotNull(r.getTipologia());
+    }
+    @Test
+    void testCheckInsertRivista() throws SQLException {
+        info[0]=RBRIVISTADAINSERIRE.getString("info[0]");
+        info[1]=RBRIVISTADAINSERIRE.getString("info[1]");
+        info[2]=RBRIVISTADAINSERIRE.getString("info[2]");
+        info[3]=RBRIVISTADAINSERIRE.getString("info[3]");
+        info[4]=RBRIVISTADAINSERIRE.getString("info[4]");
+        r.setDescrizione(RBRIVISTADAINSERIRE.getString("descrizione"));
+        r.setDataPubb(LocalDate.of(2022, 11,12));
+        r.setDisp(Integer.parseInt(RBRIVISTADAINSERIRE.getString("disponibilita")));
+        r.setPrezzo(Float.parseFloat(RBRIVISTADAINSERIRE.getString("prezzo")));
+        r.setCopieRim(Integer.parseInt(RBRIVISTADAINSERIRE.getString("copieRimanenti")));
+        assertTrue(cAP.checkDataR(info, r.getDataPubb(), r.getDisp(),r.getPrezzo() , r.getCopieRim(), r.getDescrizione()));
+
+    }
+
+    @Test
+    void testTotaleR() throws SQLException, IdException {
+        vis.setTypeAsMagazine();
+        r.setTitolo(RBSETTAOGGETTO.getString("titoloR"));
+        vis.setId(Integer.parseInt(RBSETTAOGGETTO.getString("idR")));
+        r.setCopieRim(Integer.parseInt(RBSETTAOGGETTO.getString("copieRimanentiR")));
+        vis.setQuantita(Integer.parseInt(RBSETTAOGGETTO.getString("quantitaR")));
+        r.setDisp(Integer.parseInt(RBSETTAOGGETTO.getString("disponibilita")));
+        assertNotEquals(0,cA.totale1(vis.getType(),r.getTitolo(),r.getDisp(),vis.getQuantita()));
+    }
+
+    @Test
+    void testGetRivistaById() throws SQLException {
+        assertNotNull(cMP.getRivistaById(5));
+    }
+
+    @Test
+    void testAggiungiCartaDBR() throws SQLException, IdException {
+        vis.setSpesaT((float)14.9);
+        vis.setTypeAsMagazine();
+        Date data= Date.valueOf("2028-02-22");
+        cPCC.aggiungiCartaDB(REBARTADICREDITO.getString("nome2"),REBARTADICREDITO.getString("cognome2"),REBARTADICREDITO.getString("numero2"), data, REBARTADICREDITO.getString("civ2"), Float.parseFloat(REBARTADICREDITO.getString("prezzo2")));
+        assertNotNull(data);
+    }
+
+    @Test
+    void testSetDescrizione() throws SQLException {
+        Rivista r=new Rivista();
+        r.setTitolo("Focus");
+        rD.getDesc(r);
+        int id=rD.retId(r);
+        r.setId(id);
+        String nome=rD.getNome(r);
+        int disp=rD.getDisp(r);
+        String titolo=rD.getTitolo(r);
+        assertNotEquals(0,r.getId());
+    }
+    @Test
+    void testGetLista() throws SQLException {
+        vis.setTypeAsMagazine();
+        assertNotNull(cC.getLista(vis.getType()));
+    }
+    @Test
+    void testDisponibilita() throws SQLException, IdException {
+        vis.setTypeAsMagazine();
+        assertTrue(cC.disponibilita(vis.getType(),"1"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints= {1,2,3,4,5,6})
+    void testScaricaRivista(int ints) throws DocumentException, IOException, URISyntaxException {
+        vis.setTypeAsMagazine();
+        vis.setId(ints);
+        cD.scarica(vis.getType());
+        assertEquals(ints,vis.getId());
+    }
+    @Test
+    void testControlla() {
+        assertDoesNotThrow(()->cPCash.controlla(RBFATTURA.getString("nome1"), RBFATTURA.getString("cognome1"),RBFATTURA.getString("via"),RBFATTURA.getString("comunicazioni1")));
+
+    }
+
+
+}
+
+
+
---
+++ target/LibreriaMaven/cartaCredito.jsp
@@ -0,0 +1,147 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+     
+    
+<!DOCTYPE html>
+<html lang=it>
+<head>
+<link rel="stylesheet" href="css//cartaCredito.css">
+<meta charset="UTF-8">
+<title>Carta Credito page</title>
+</head>
+<body>
+<h1>Benvenuto nella schermata del pagamento con carta di credito</h1>
+
+<h2>Inserire le credenziali</h2>
+
+
+
+
+<form action="CartaCreditoServlet" method="post">
+<table>
+<caption></caption>
+<tr>
+<th scope="col">
+</th>
+<tr>
+<td>
+inserire nome 
+</td>
+<td>
+</td>
+<td>
+<input type="text" id="nomeL" name="nomeL" >
+</td>
+</tr>
+<tr>
+<td>
+inserire cognome 
+</td>
+<td>
+</td>
+<td>
+<input type="text" id="cognomeL" name="cognomeL" >
+</td>
+</tr>
+<tr>
+<td>
+inserire codice carta 
+</td>
+<td>
+xxxx-xxxx-xxxx-xxxx
+</td>
+<td>
+<input type="text" id="cartaL" name="cartaL" >
+</td>
+</tr>
+<tr>
+<td>
+inserire data scadenza 
+</td>
+<td>
+yyyy/mm/dd (usare '/')
+</td>
+<td>
+<input type="text" id="scadL" name="scadL" >
+</td>
+</tr>
+<tr>
+<td>
+inserire pin 
+</td>
+<td>
+xyz
+</td>
+<td>
+<input type="password" id="passL" name="passL" >
+</td>
+</tr>
+<tr>
+<td>
+<br>
+</td>
+<td>
+<input type="submit" id="buttonI" name="buttonI" value="paga" class="invia">
+</td>
+<td>
+<input type="submit" id="buttonA" name="buttonA" value="annulla" class="annulla">
+</td>
+</table>
+
+<br>
+<br>
+
+<c:set var = "tipo" scope = "session" value = "${bean1.getIsLoggedB() }"/>
+
+<c:choose>
+<c:when test="${ tipo==true}">
+
+<div>
+<input type="submit" id="regB" name="regB" value="registra e paga" class="registra">
+</div>
+
+<br>
+<br>
+<table>
+<caption>elenco carte </caption>
+<tr>
+<th scope="col"></th>
+<tr>
+<tr>
+<td>
+numero carta 
+</td>
+<!--  mette	re foreach by database -->
+<c:forEach items="#{beanL.listaCarteCreditoB }" var="lista">
+
+
+<tr>
+
+
+<td>${ lista.getNumero() }</td>
+
+</tr>
+</c:forEach>
+</table>
+
+<br>
+<div>
+
+inserire nome utente :
+
+<input type="text" name="utenteL" id="utenteL">
+
+<input type="submit" name="prendiDB" id="prendiDB" value="genera lista">
+</div>
+
+
+</c:when>
+</c:choose>
+
+</form>
+
+
+
+</body>
+</html>
